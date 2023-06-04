efe4f3abcc31f9d8e257d0c8545a4b0856b6c3a9
phn53369 <nguyet-ha1.phung@stu.uni-regensburg.de>
2023-06-04 21:23:46 +0200

    aufgabe 1a

--- main.py
+++ main.py
@@ -3,39 +3,59 @@ import requests
 import ast
 
 ELASTICSEARCH_URL = 'http://localhost:9200/'
-ORIGINAL_FILE_PATH = 'sample-1M.jsonl'
-EXTRACTED_DATA_FILE_PATH = 'articles_data.json'
+ORIGINAL_FILE_PATH = 'data/sample-1M.jsonl'
+EXTRACTED_DATA_FILE_PATH = 'data/extracted_data.json'
+ARTICLES_DATA_FILE_PATH = 'data/articles_data.json'
 
-# Aufgabe 1
+extracted_data_list = []
+
+
+# AUFGABE 1
+# Format JSON File
+def format_json_file(file_path, new_path):
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+
+    # Add new line after the closing curly braces
+    file_content = file_content.replace('},', '}\n')
+
+    # Remove the square brackets
+    for char in ['[', ']']:
+        if char in file_content:
+            file_content = file_content.replace(char, '')
+
+    # Remove spaces at the beginning of each line in a JSON file
+    file_content = '\n'.join([line.lstrip() for line in file_content.splitlines()])
+
+    with open(new_path, 'w') as file:
+        file.write(file_content)
+
+
 # Extract the first 10_000 data
-# since sample-1M.jsonl is too large to push into Github, so I deleted this file
-
-
-def extract_the_first_10000_data():
-    first_10000_data = []
+def extract_data(data_list):
 
     with open(ORIGINAL_FILE_PATH, 'r') as original_file:
         for line in original_file:
             try:
                 data = json.loads(line)
-                first_10000_data.append(data)
+                data_list.append(data)
             except json.JSONDecodeError as e:
                 print(f"Error parsing line: {line}. {e}")
 
-    # Get the first 10 objects
-    first_10000_data = first_10000_data[:10000]
+    # Get the first 10_000 objects
+    data_list = data_list[:5]
 
-    # Write the first 10 objects to the new JSON file
+    # Write the first 10_000 objects to the new JSON file
     with open(EXTRACTED_DATA_FILE_PATH, 'w') as new_file:
-        json.dump(first_10000_data, new_file, indent=4)
+        json.dump(data_list, new_file)
 
-    print('First 10000 data have been extracted and saved to the new articles_data.json file.')
+    # Format file for indexing
+    format_json_file(EXTRACTED_DATA_FILE_PATH, ARTICLES_DATA_FILE_PATH)
+
+    print('First 10_000 data have been extracted and saved to the articles_data.json file.')
 
 
-extract_the_first_10000_data()
-
-
-# Create index
+# Create index method
 def create_index(index_name, mappings):
     response = requests.request(method='PUT',
                                 url=ELASTICSEARCH_URL + index_name,
@@ -55,5 +75,29 @@ mappings = {
         }
     }
 }
-#create_index(index_name='articles_data', mappings=mappings)
 
+
+# Index method
+def index_docs(document_dicts):
+    for idx, doc in enumerate(document_dicts):
+        response = requests.request(method='POST',
+                                    url=ELASTICSEARCH_URL + 'articles_data/_create/_' + str(idx),
+                                    json=doc)
+        print(response.json())
+
+
+if __name__ == "__main__":
+    extract_data(extracted_data_list)
+
+    # Aufgabe 1a: Index for the first 10_000 articles
+    create_index(index_name='articles_data', mappings=mappings)
+
+    with open(ARTICLES_DATA_FILE_PATH, 'r') as json_file:
+        articles_data = json_file.readlines()
+
+    articles_data = [ast.literal_eval(line) for line in articles_data]
+    index_docs(articles_data)
+
+    print(len(extracted_data_list))
+
+    # Aufgabe 1b: Index for the first 10_000 articles with Pre-processing step
